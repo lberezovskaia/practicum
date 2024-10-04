dce6536be2aabe308451bd4c8bba61c9ccd7a36b
lberezovskaia <berezovskayayelizaveta@gmail.com>
2024-10-05 02:06:35 +0300

    3.27.c

--- 3.27.c
+++ 3.27.c
@@ -3,23 +3,24 @@
 
 int main(void)
 {
-  long int n = 1959785;
-  long int ans;
-  int a[100];
-  int i = 0;
-while (n != 0)
-{
-if (((n % 10) != 0) && ((n % 10) != 5))
-{
-  i++;
-  a[i] = n % 10;
-};
-  n = n / 10;
-};
-for (i; i>0; i--)
-{
-  ans = ans + (a[i] * pow(10,i-1));;
-};
-printf("%ld\n", ans);
-return 0;
+    long int n = 0;
+    scanf("%ld", &n);
+    long int ans;
+    int a[100];
+    int i = 0;
+    while (n != 0)
+    {
+        if (((n % 10) != 0) && ((n % 10) != 5))
+        {
+            i++;
+            a[i] = n % 10;
+        };
+         n = n / 10;
+    };
+    for (i; i > 0; i--)
+    {
+        ans = ans + (a[i] * pow(10, i-1));;
+    };
+    printf("%ld\n", ans);
+    return 0;
 }
