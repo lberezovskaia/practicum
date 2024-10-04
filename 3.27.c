78ee384af87f6e9a01b136779849d8f2c0486466
lberezovskaia <berezovskayayelizaveta@gmail.com>
2024-10-04 17:38:30 +0300

    3.27.c

---
+++ 3.27.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+  long int n = 1959785;
+  long int ans;
+  int a[100];
+  int i = 0;
+while (n != 0)
+{
+if (((n % 10) != 0) && ((n % 10) != 5))
+{
+  i++;
+  a[i] = n % 10;
+};
+  n = n / 10;
+};
+for (i; i>0; i--)
+{
+  ans = ans + (a[i] * pow(10,i-1));;
+};
+printf("%ld\n", ans);
+return 0;
+}
