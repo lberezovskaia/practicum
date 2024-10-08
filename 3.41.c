9cd3ad422c5a3d46b13fdca8219708bda6bd3d40
lberezovskaia <berezovskayayelizaveta@gmail.com>
2024-10-09 01:59:41 +0300

    3.41.c

---
+++ 3.41.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+double eps; 
+scanf("%lf", &eps);
+double a = 0.0;
+double b = sqrt(0.5); 
+double approx; 
+long double pi;
+approx = b;
+while (fabs(b - a) > eps)
+{
+a = b;
+b = sqrt((0.5) + (0.5 * a));
+approx = approx * b;
+}
+pi = (2.0) / approx;
+printf("%Lf\n", pi);
+return 0;
+}
