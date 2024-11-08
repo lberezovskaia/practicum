90f54d7700e411f01354cd0000ccafe12a3c3b9c
lberezovskaia <berezovskayayelizaveta@gmail.com>
2024-11-08 17:00:54 +0300

    Create 6.42.c

---
+++ task3/6.42.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue{
+    double data;
+    struct queue *next;
+};
+
+void insert(struct queue **head, double data){
+    if ((*head) == NULL){
+        (*head) = (struct queue *)malloc(sizeof(struct queue));
+        (*head)->data = data;
+        (*head)->next = NULL;
+    }
+    else{
+        struct queue *tmp = (*head);
+        while (tmp->next != NULL){
+            tmp = tmp->next;
+        }
+        tmp->next = (struct queue *)malloc(sizeof(struct queue));
+        tmp = tmp->next;
+        tmp->data = data;
+        tmp->next = NULL;
+    }
+}
+
+double extract(struct queue **head){
+    double elem;
+    elem = (*head)->data;
+    if ((*head) == NULL) (*head) = NULL; 
+    struct queue *tmp = (*head)->next; 
+    (*head) = tmp;
+    return elem;
+}
+
+void print_queue(struct queue *head){
+    struct queue *tmp = head;
+    printf("queue:");
+    while (tmp != NULL) {
+        printf("   %lf", tmp->data);
+        tmp = tmp->next; 
+    }
+    printf("\n");
+    printf("\n");
+}
+
+int main(void){
+    struct queue *head = NULL;
+    double n;
+    printf("enter double number: ");
+    scanf("%lf", &n);
+    insert(&head, n);
+    print_queue(head);
+    
+    printf("enter double number: ");
+    scanf("%lf", &n);
+    insert(&head, n);
+    print_queue(head);
+
+    printf("enter double number: ");
+    scanf("%lf", &n);
+    insert(&head, n);
+    print_queue(head);
+    
+    double elem;
+    elem = extract(&head);
+    printf("extract elem: %lf\n", elem);
+    print_queue(head);
+
+    elem = extract(&head);
+    printf("extract elem: %lf\n", elem);
+    print_queue(head);
+
+    elem = extract(&head);
+    printf("extract elem: %lf\n", elem);
+    print_queue(head);
+
+    insert(&head, 5);
+    print_queue(head);
+
+    return 0;
+}
